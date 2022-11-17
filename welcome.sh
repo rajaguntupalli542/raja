c25f7eb194b3252f91460c1f7691d46e6a1121bf
rajaguntupalli542 <54567632+rajaguntupalli542@users.noreply.github.com>
2022-11-17 16:11:57 +0530

    creating index

--- welcome.sh
+++ welcome.sh
@@ -1,4 +1,14 @@
-echo "Hello World"
-echo "Test"
-
-
+curl --user elastic:aIEM29L3QmI7Etd9zwFJHdvs -XPUT "https://my-deployment-377e7b.es.us-east-2.aws.elastic-cloud.com/ci-cd-test" -H "kbn-xsrf: reporting" -H "Content-Type: application/json" -d'
+{
+ "settings": {
+   "number_of_shards": 1,
+   "number_of_replicas": 0
+ },
+ "mappings": {
+   "properties": {
+     "name":{
+       "type": "text"
+     }
+   }
+ }
+}'
