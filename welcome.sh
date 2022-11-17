d976d78b3fe95b3dabc5ce4e21a1bc19242385f5
rajaguntupalli542 <54567632+rajaguntupalli542@users.noreply.github.com>
2022-11-17 16:14:40 +0530

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,4 +1,4 @@
-curl --user elastic:aIEM29L3QmI7Etd9zwFJHdvs -XPUT "https://my-deployment-377e7b.es.us-east-2.aws.elastic-cloud.com/ci-cd-test" -H "kbn-xsrf: reporting" -H "Content-Type: application/json" -d'
+curl --user elastic:$1 -XPUT "https://my-deployment-377e7b.es.us-east-2.aws.elastic-cloud.com/ci-cd-test" -H "kbn-xsrf: reporting" -H "Content-Type: application/json" -d'
 {
  "settings": {
    "number_of_shards": 1,
